1bba2bdaa8c03c0b35dc8c87db935a239383abb6
Vatsal Sanchala <bevatsal1122@gmail.com>
2025-03-30 13:10:54 +0530

    fix: reddis deploy

--- src/lib/queue.ts
+++ src/lib/queue.ts
@@ -53,8 +53,8 @@ const initializeRedis = async () => {
   // First, try connecting with connection string if provided
   if (REDIS_URL) {
     try {
-    //   const client = await tryConnect(REDIS_URL, "connection string");
-    //   if (client) return client;
+      const client = await tryConnect(REDIS_URL, "connection string");
+      if (client) return client;
     } catch (error) {
       console.warn('Connection with REDIS_URL failed, will try other methods');
     }
